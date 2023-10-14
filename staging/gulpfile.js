6844706393ba28328192e10cffac1edfd32f2f1c
Douglas Martin <mdgeek1@gmail.com>
2023-10-13 20:55:22 -0400

    Use gulp and rollup for builds.

---
+++ staging/gulpfile.js
@@ -0,0 +1,33 @@
+const {src, dest, series} = require('gulp');
+const minifyJS = require('gulp-minify');
+
+const srcDir = '${src.rootdir}/';
+const destDir = '${webjar.target}/';
+
+function task1() {
+    return _minifyJS('jquery.qrcode.js', 'dist')
+}
+
+function _toSrc(_src) {
+    return src(_src, {allowEmpty: false, cwd: srcDir});
+}
+
+function _toDest(_dest) {
+    return dest(_dest || '.', {cwd: destDir});
+}
+
+function _minifyJS(_src, _dest) {
+    console.log('  Minifying ' + _src);
+    return _toSrc(_src)
+        .pipe(_toDest(_dest))
+        .pipe(minifyJS(
+            {
+                noSource: true,
+                ext: {
+                    min: '.min.js'
+                }
+            }))
+        .pipe(_toDest(_dest))
+}
+
+exports.default = task1;
